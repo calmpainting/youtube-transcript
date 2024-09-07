db06e0e7ce723b7feaf2cb9ec28a2b6686ccef44
calmpainting <heaven.a@protonmail.com>
2024-09-07 04:14:33 +0200

    Changes to setup.py, easier setup for users

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
+import subprocess
 
 class CustomInstallCommand(install):
     def run(self):
         install.run(self)
         self.create_env_file()
+        self.install_requirements()
 
     def create_env_file(self):
         env_path = os.path.join(os.getcwd(), '.env')
         if not os.path.exists(env_path):
+            api_key = input("Please enter your API_KEY: ")
             with open(env_path, 'w') as env_file:
-                env_file.write("API_KEY=\n")
-            print(".env file created. Please add your API_KEY.")
+                env_file.write(f"API_KEY={api_key}\n")
+            print(".env file created with your API_KEY.")
+
+    def install_requirements(self):
+        requirements_path = os.path.join(os.getcwd(), 'requirements.txt')
+        if os.path.exists(requirements_path):
+            subprocess.check_call(['pip', 'install', '-r', requirements_path])
+            print("Requirements installed from requirements.txt.")
 
 setup(
     name='youtube_transcript_fetcher',
